525aaaf8f692bd8b56707c2d94c05af32db27523
Nadrojria <loicdesbordes@yahoo.fr>
2025-03-07 17:37:24 +0100

    step 2 : OK

--- script.js
+++ script.js
@@ -1,9 +1,73 @@
+const latinToMorse = {
+	'A':'.-',
+	'B':'-...',
+	'C':'-.-.',
+	'D':'-..',
+	'E':'.',
+	'F':'..-.',
+	'G':'--.',
+	'H':'....',
+	'I':'..',
+	'J':'.---',
+	'K':'-.-',
+	'L':'.-..',
+	'M':'--',
+	'N':'-.',
+	'O':'---',
+	'P':'.--.',
+	'Q':'--.-',
+	'R':'.-.',
+	'S':'...',
+	'T':'-',
+	'U':'..-',
+	'V':'...-',
+	'W':'.--',
+	'X':'-..-',
+	'Y':'-.--',
+	'Z':'--..'
+}
+
+const morseToLatin = {
+    '-': "T",
+    '--': "M",
+    '---': "O",
+    '--.': "G",
+    '--.-': "Q",
+    '--..': "Z",
+    '-.': "N",
+    '-.-': "K",
+    '-.--': "Y",
+    '-.-.': "C",
+    '-..': "D",
+    '-..-': "X",
+    '-...': "B",
+    '.': "E",
+    '.-': "A",
+    '.--': "W",
+    '.---': "J",
+    '.--.': "P",
+    '.-.': "R",
+    '.-..': "L",
+    '..': "I",
+    '..-': "U",
+    '..-.': "F",
+    '...': "S",
+    '...-': "V",
+    '....': "H"
+}
 
 function getLatinCharacterList (text) {
     let result = text.split("");
     console.log(result);
 }
 
-let test = "Hello, World";
+function translateLatinCharacter (character) {
+    for (const property in latinToMorse) {
+        if (property == character.toUpperCase()) {
+            console.log(latinToMorse[property]);
+        }
+    }
+}
 
-getLatinCharacterList(test);
+getLatinCharacterList("Hello, world");
+translateLatinCharacter("o");
